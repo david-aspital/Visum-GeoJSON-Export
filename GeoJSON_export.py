515b9ff7b339b5b4ae3ebb269aa512fd91472702
david-aspital <83019108+david-aspital@users.noreply.github.com>
2021-06-10 17:34:00 +0100

    Further development

--- GeoJSON_export.py
+++ GeoJSON_export.py
@@ -1,22 +1,42 @@
-import pandas as pd
 import json
+import sys
+import os
+import glob
+import tempfile
+import pandas as pd
+import win32com.client
 import shapely.wkt
 import shapely.geometry
+from pathlib import Path
+import exports as exp
+import dialogs as dlg
+import xml.etree.ElementTree as ET
 
+Visum = None
 
+wkt_col_names = ['WKTPOLYWGS84', 'WKTLOCWGS84', 'WKTSURFACEWGS84']
+visum_lists = {'ZONE' : Visum.Lists.CreateZoneList,
+               'LINK' : Visum.Lists.CreateLinkList,
+               'NODE' : Visum.Lists.CreateNodeList}
 
 def wkt2geometry(df, wkt_col):
+    # Create list of WKT values, apply conversion to geometry and create new column
     wkt_list = df[wkt_col].tolist()
     out_list = []
     for s in wkt_list:
         out_list.append(json.dumps(shapely.geometry.mapping(shapely.wkt.loads(s))))
-    df['Geometry'] = out_list
+    df[f'Geom_{wkt_col}'] = out_list
     return df
 
 def cols2properties(df, cols=None):
+    # Convert data columns to properties
     if cols is None:
         cols = df.columns.values.tolist()
-        cols.remove('WKTSURFACE')
+        for col in wkt_col_names:
+            try:
+                cols.remove(col)
+            except  ValueError:
+                pass
     df['Properties'] = ""
     for i, row in df.iterrows():
         properties = {}
@@ -25,18 +45,93 @@ def cols2properties(df, cols=None):
         df.loc[i, 'Properties'] = json.dumps(properties)
     return df
 
-def rows2features(df):
-    df['Feature'] = '{"type": "Feature", "geometry": '+df.Geometry+', "properties": '+df.Properties+'}'
+def rows2features(df, wkt_col):
+    # Create features from geometries and properties
+    df[f'Feat_{wkt_col}'] = '{"type": "Feature", "geometry": '+df[f'Geom_{wkt_col}']+', "properties": '+df.Properties+'}'
     return df
-    
 
-df = pd.read_csv('Test_data.att', sep='\t', encoding='latin1')
-df2 = cols2properties(df)
-df3 = wkt2geometry(df2,'WKTSURFACE')
-df4 = rows2features(df3)
-feature_list = df4['Feature'].tolist()
-feature_collection = '{"type": "FeatureCollection",  "features": ['+','.join(feature_list)+']}'
-test = json.loads(feature_collection)
-with open('Test.geojson', 'w') as f:
-    f.write(feature_collection)
+def get_llax_files(folder):
+    # Get list of all .llax files in a folder
+    llax_files = [f for f in glob.glob(f'{folder}\\*.llax')]
+    return llax_files
+
+def get_visum_object(llax):
+    # Get Visum object from .llax file
+    root = ET.parse(llax).getroot()
+    for tag in root.findall('listLayoutItem/listLayoutCommonEntries'):
+        obj = tag.get('layoutIdentifier').replace('LIST_LAYOUT_')
+    return obj
+
+def get_wkt_cols(df):
+    # Finds WKT columns in the dataframe
+    cols = df.columns.values.tolist()
+    wkt_cols = [x for x in cols if x in wkt_col_names]
+
+    if wkt_cols == []:
+        raise KeyError("No valid WKT columns found. Please check the layout file and ensure that the WGS84 variants are used.")
+    return wkt_cols
+
+def process_llax(llax, Visum, temp_path, filter=False):
+    obj = get_visum_object(llax)
+    df = exp.create_data_frame(visum_lists[obj], llax, temp_path, filter)
+    wkt_cols = get_wkt_cols(df)
+    df = cols2properties(df)
+    for col in wkt_cols:
+        df = wkt2geometry(df, col)
+        df = rows2features(df, col)
+        feature_list = df[f'Feat_{col}'].tolist()
+        feature_collection = '{"type": "FeatureCollection",  "features": ['+','.join(feature_list)+']}'
+        output = json.loads(feature_collection)
+        if len(wkt_cols) > 1:
+            out_name = f'{llax}_{col.replace("WKT","").replace("WGS84", "")}.geojson'
+        else:
+            out_name = f'{llax}.geojson'
+        with open(out_name, 'w') as f:
+            f.write(json.dumps(feature_collection, indent=4, sort_keys=True))
+
+
+def export_from_current(temp_path):
+    sys.path.append(f"C:\\Users\\{os.getusername()}\\Anaconda3\\envs\\GeoJSON\\Lib\\site-packages")
+
+    llax_folder = os.path.join(os.path.dirname(Visum.IO.CurrentVersionFile), 'GeoJSON llax')
+
+    # Get llax files for processing
+    llax_files = get_llax_files(llax_folder)
+
+    # Loop through llax files and export data
+    for llax in llax_files:
+        process_llax(llax, Visum, temp_path)
+
+def export_from_version(visum_versionNo, temp_path):
+    # 
+
+    # Select ver path and llax folder path
+    ver_path = dlg.file_select_dlg("Please select a Visum version file...", "Version |*.ver")
+    llax_folder = dlg.folder_select_dlg("Please select a folder containing layout files...",os.curdir())
+
+    # Get llax files for processing
+    llax_files = get_llax_files(llax_folder)
+    
+    # Launch Visum 
+    Visum = win32com.client.Dispatch(f"Visum.Visum.{visum_versionNo}")
+    Visum.IO.LoadVersion(ver_path)
+
+    # Loop through llax files and export data
+    for llax in llax_files:
+        process_llax(llax, Visum, temp_path)
+
+    # Close Visum to exit
+    Visum = None
+
+
+if __name__ == "__main__":
+
+    temp_path = f"{tempfile.gettempdir()}\\Visum_GeoJSON"
+    Path(temp_path).mkdir(parents=True, exist_ok=True)
+
+    if 'Visum' in globals():
+        export_from_current(Visum=Visum, temp_path=temp_path)
+    else:
+        export_from_version(visum_versionNo=21, temp_path=temp_path)
+
 
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Visum-GeoJSON-Export
-Script to export data from Visum into GeoJSON format
+# PTV Visum GeoJSON Export
+Tool to export data from PTV Visum into GeoJSON format. Please contact [David Aspital](mailto:david.aspital@ptvgroup.com?subject=%5BGitHub%5D%20GeoJSON%20Export%20Tool), or log an issue if you have any queries about this tool.
+
+
+## Introduction
+
+GeoJSON is a format for encoding a variety of geographic data structures that is becoming increasingly popular as an alternative to ESRI Shapefiles. The full specification for GeoJSON can be found [here](https://datatracker.ietf.org/doc/html/rfc7946), but it is important to note that it is based on the World Geodetic System 1984 projection system.
+
+PTV Visum does not currently offer a built-in exporter for data in GeoJSON format, so this tool has been developed as an add-in to pull data from a Visum .ver file using layout (.llax) files to define the data to be exported.
+
+
+## Requirements
+The tool requires the use of the `shapely` Python module which is not currently available with the standard PTV Visum installation. In order for the tool to run successfully, it is required that a Python environment is created that includes this module. It is suggested that [Anaconda](https://www.anaconda.com/) is used for this and `shapely` is installed in the environment.
+
+This tool has been designed to work with Visum 2021, but can be simply updated to work with other versions of Visum. Users of this tool are assumed to be familiar with PTV Visum and Transport Modelling terminology. For more info about PTV Visum, please visit: https://www.ptvgroup.com/en/solutions/products/ptv-visum/.
+
+
+## Running the Tool
+There are two options for running the tool:
+1. Within Visum as part of a model run 
+2. External to Visum after a model has been run and saved
+
+### Within Visum
+
+The first option is designed to require minimal user intervention and be set up as a part of a standard model running procedure. It can be set up as a 'Run script' procedure that links to the `GeoJSON.py` file, with the layout files that define the data to be exported saved in a 'GeoJSON Layouts' folder in the same location as the script file. The only user intervention required to get the script to run is to update the folder path on **<span style="color:red"> line X </span>** with the location of the Python environment that contains `shapely`. The script will then export the data into the same folder as the version file.
+
+
+### Outside of Visum
+The second option is for processing data from a model that has already been run and saved. The script can be run externally to Visum from the environment in which `shapely` is installed. A file selection dialog will first appear in which the user should select the Visum version file, before a folder selection where the user can select the folder containing the layout files defining the data to be exported. The tool will then run and export the data into the same folder as the script.
